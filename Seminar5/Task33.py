d45a4d0ff03570bec21d3d331220752bb2f1afd0
Мега Крутой <krutoi.chuvak1918@gmail.com>
2023-12-25 00:48:30 +0300

    V1

--- Seminar5/Task33.py
+++ Seminar5/Task33.py
@@ -3,12 +3,14 @@
 # Напишите программу, которая заменяет оценки Василия, но
 # наоборот все максимальные - на минимальные.
 
-def func_1(lst):
-    max_n = max(lst)
-    for i in lst:
-        if i == max_n:
-            i = 1
-    return lst
+lst1 = [1, 3, 3, 3, 4]
+def func_2(lst, lst_new=[], min_n=min(lst1), max_n=max(lst1)):
+    if len(lst) == 0:
+        return lst_new
+    elif lst[0] == max_n:
+        lst_new.append(min_n)
+    else:
+        lst_new.append(lst[0])
+    return func_2(lst[1:])
 
-lst = (1, 3, 3, 3, 1)
-print(func_1(lst))
+print(func_2(lst1))
